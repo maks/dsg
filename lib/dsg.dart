32bc1f869ffd642bea5db1c41c3ff34bc64f7a1f
Maksim Lin <maks@manichord.com>
2020-07-16 20:41:27 +1000

    fix formatting

--- lib/dsg.dart
+++ lib/dsg.dart
@@ -10,7 +10,7 @@ import 'package:console_log_handler/print_log_handler.dart';
 import 'package:http_server/http_server.dart';
 import 'package:intl/intl.dart';
 import 'package:logging/logging.dart';
-import 'package:markdown/markdown.dart'as md;
+import 'package:markdown/markdown.dart' as md;
 import 'package:packages/packages.dart';
 import 'package:path/path.dart' as path;
 import 'package:reflected_mustache/mustache.dart' as mustache;
